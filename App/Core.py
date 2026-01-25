de1777021b292e39e551288b2d649ac564e665ad
orex2105 <pokomisa55@gmail.com>
2026-01-25 18:11:19 +0300

    класс птицы

--- App/Core.py
+++ App/Core.py
@@ -1,8 +1,6 @@
-from abc import ABC, abstractmethod
-
-from PySide6.QtCore import QRectF, QPointF
+from PySide6.QtCore import QRectF, QPointF, Qt
 from PySide6.QtGui import QPixmap, QPainter, QIcon
-from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLayoutItem
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLayoutItem, QGraphicsPixmapItem
 
 from App.Scenes import Menu
 
@@ -59,68 +57,20 @@ class MainWindow(QMainWindow):
                 widget.deleteLater()
 
 
-class Sprite(ABC):
-    def __init__(self, image_path: str | None, x: float=0, y: float=0) -> None:
-        # Накладываем текстуру спрайту, если путь к изображению задан
-        self.image_path = image_path
-        if image_path:
-            self.pixmap = QPixmap(image_path)
-        else:
-            # Если изображения нет, создаем пустое изображение-заглушку размером 64x64
-            self.pixmap = QPixmap(64, 64)
-        # Прямоугольник спрайта
-        self.rect = QRectF(x, y, self.pixmap.width(), self.pixmap.height())
-        self._visible: bool = True
-        self._angle: float = 0.0
-    
-    @abstractmethod
+class Bird(QGraphicsPixmapItem):
+    def __init__(self, pixmap, x, y) -> None:
+        super().__init__(pixmap)
+        self.setPos(x, y)
+        self.setTransformationMode(Qt.TransformationMode.SmoothTransformation)
+
+        self.speed_y = 0
+        self.gravity = 0.5
+        self.flap_power = -9.0
+        self.max_fall_speed = 12.0
+
     def update(self) -> None:
-        pass
-
-    def move_to(self, x: float, y: float) -> None:
-        self.rect.moveTo(x, y)
-
-    def draw(self, painter: QPainter) -> None:
-        if not self._visible:
-            return None
-
-        # Сохранение текущего состояния рисовальщика
-        painter.save()
-        # Поворот рисовальщика относительно центра спрайта
-        painter.translate(self.rect.center())
-        # Поворот на заданный угол
-        painter.rotate(self._angle)
-
-        # Начальная точка рисования спрайта по x и y
-        starting_x: int = int(-self.rect.width() / 2)
-        starting_y: int = int(-self.rect.height() / 2)
-        # Рисование спрайта
-        painter.drawPixmap(starting_x, starting_y, self.pixmap)
-    
-    @property
-    def x(self) -> float:
-        return self.rect.x()
-
-    @property
-    def y(self) -> float:
-        return self.rect.y()
-
-    @property
-    def center(self) -> QPointF:
-        return self.rect.center()
-
-    @property
-    def angle(self) -> float:
-        return self._angle
-
-    @angle.setter
-    def angle(self, a: float) -> None:
-        self._angle = a
-
-    @property
-    def visible(self) -> bool:
-        return self._visible
-
-    @visible.setter
-    def visible(self, v: bool) -> None:
-        self._visible = v
+        self.speed_y += self.gravity
+        self.speed_y = min(self.speed_y, self.max_fall_speed)
+        
+        new_y = self.y() + self.speed_y
+        self.setPos(self.x(), new_y)
