64aaa67f1d6d4d3b703e63a0149fd785209edebf
orex2105 <pokomisa55@gmail.com>
2026-01-25 17:56:22 +0300

    рефакторинг проекта

--- App/Core.py
+++ App/Core.py
@@ -1,6 +1,63 @@
-from PySide6.QtCore import QRectF, QPointF
-from PySide6.QtGui import QPixmap, QPainter
 from abc import ABC, abstractmethod
+
+from PySide6.QtCore import QRectF, QPointF
+from PySide6.QtGui import QPixmap, QPainter, QIcon
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLayoutItem
+
+from App.Scenes import Menu
+
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.set_title("Flappy Bird")
+        self.set_window_size("assets/sprites/background-day.png")
+        self.set_icon("assets/icon.ico")
+
+        # Центральный виджет
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        # Создание основного вертикального лэйаута
+        self.main_layout = QVBoxLayout(self.central_widget)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+        self.main_layout.setSpacing(0)
+
+        self.game_state = "menu"
+
+        self.show_menu()
+    
+    def set_window_size(self, background_path: str) -> None:
+        size = QPixmap(background_path).size()
+        self.setFixedSize(size.width(), size.height())
+    
+    def set_icon(self, icon_path: str) -> None:
+        self.setWindowIcon(QIcon(icon_path))
+    
+    def set_title(self, title) -> None:
+        self.setWindowTitle(title)
+
+    def show_menu(self) -> None:
+        # Очищение текущего виджета
+        self.clear_current_widget()
+
+        self.game_state = "menu"
+
+        # Сцена меню
+        menu_scene = Menu()
+        self.main_layout.addWidget(menu_scene)
+
+    def clear_current_widget(self) -> None:
+        # Удаление текущего виджета из лэйаута
+        if self.main_layout.count() > 0:
+            # Берем первый элемент из лэйаута
+            item: QLayoutItem = self.main_layout.takeAt(0)
+            # Удаляем виджет, если он существует
+            widget = item.widget()
+            if widget:
+                widget.deleteLater()
+
 
 class Sprite(ABC):
     def __init__(self, image_path: str | None, x: float=0, y: float=0) -> None:
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-from core.ui.main_window import MainWindow
+from App.Core import MainWindow
 
 
 def main() -> None:
